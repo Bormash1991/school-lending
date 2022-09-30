75ec60cfe098d304521f910d159922b9e122491a
Lutsenko Bohdan <lutsenko2004@gmail.com>
2022-09-30 21:55:42 +0300

    corrected js

--- html_css/js/mobile-menu.js
+++ html_css/js/mobile-menu.js
@@ -5,18 +5,18 @@ window.addEventListener("DOMContentLoaded", () => {
     link = document.querySelector(".header__list");
 
   humburger.addEventListener("click", () => {
-    document.body.style.overflow = "hidden";
+    // document.body.style.overflow = "hidden";
     nav.classList.add("header__nav_active");
   });
   cross.addEventListener("click", () => {
-    document.body.style.overflow = "unset";
+    // document.body.style.overflow = "unset";
     nav.classList.remove("header__nav_active");
   });
   link.addEventListener("click", (e) => {
     let target = e.target;
-   
+
     if (e.target.className == "header__link") {
-      document.body.style.overflow = "unset";
+      // document.body.style.overflow = "unset";
       nav.classList.remove("header__nav_active");
     }
   });
