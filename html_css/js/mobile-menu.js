b54cdaaa44772f36c94a6f04e55c957cc77bd7d7
Lutsenko Bohdan <lutsenko2004@gmail.com>
2022-10-05 22:35:21 +0300

    ls_5 s

--- html_css/js/mobile-menu.js
+++ html_css/js/mobile-menu.js
@@ -1,23 +1,21 @@
+let humburger = document.querySelector(".header__humburger"),
+  nav = document.querySelector(".header__nav"),
+  cross = document.querySelector(".header__cross"),
+  link = document.querySelector(".header__list");
 
-  let humburger = document.querySelector(".header__humburger"),
-    nav = document.querySelector(".header__nav"),
-    cross = document.querySelector(".header__cross"),
-    link = document.querySelector(".header__list");
+humburger.addEventListener("click", () => {
+  document.body.style.overflow = "hidden";
+  nav.classList.add("header__nav_active");
+});
+cross.addEventListener("click", () => {
+  document.body.style.overflow = "";
+  nav.classList.remove("header__nav_active");
+});
+link.addEventListener("click", (e) => {
+  let target = e.target;
 
-  humburger.addEventListener("click", () => {
-    // document.body.style.overflow = "hidden";
-    nav.classList.add("header__nav_active");
-  });
-  cross.addEventListener("click", () => {
-    // document.body.style.overflow = "unset";
+  if (e.target.className == "header__link") {
+    document.body.style.overflow = "";
     nav.classList.remove("header__nav_active");
-  });
-  link.addEventListener("click", (e) => {
-    let target = e.target;
-
-    if (e.target.className == "header__link") {
-      // document.body.style.overflow = "unset";
-      nav.classList.remove("header__nav_active");
-    }
-  });
-
+  }
+});
