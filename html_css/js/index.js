ee3fe466d0980bff3aa01c322e15155489ae5557
Lutsenko Bohdan <lutsenko2004@gmail.com>
2022-10-16 14:28:10 +0300

    ls_5_1 add slick slider for all type of device

--- html_css/js/index.js
+++ html_css/js/index.js
@@ -2,7 +2,7 @@ import { humburger } from "./mobile-menu.js";
 import { headerAppearsWithScroll } from "./header-scroll.js";
 import { paginator } from "./paginator.js";
 window.addEventListener("DOMContentLoaded", () => {
-  const data = [
+  const dataForPaginator = [
     {
       id: 1,
       title: "accusamus beatae ad facilis cum similique qui sunt",
@@ -99,32 +99,46 @@ window.addEventListener("DOMContentLoaded", () => {
       redirectLink: "https://jsonplaceholder.typicode.com",
       category: "Design",
     },
-    {
-      id: 5,
-      title: "natus nisi omnis corporis facere molestiae rerum in",
-      url: "https://via.placeholder.com/600/f66b97",
-      userImage: "https://via.placeholder.com/150/f66b97",
-      redirectLink: "https://jsonplaceholder.typicode.com",
-      category: "Design",
-    },
-    {
-      id: 5,
-      title: "natus nisi omnis corporis facere molestiae rerum in",
-      url: "https://via.placeholder.com/600/f66b97",
-      userImage: "https://via.placeholder.com/150/f66b97",
-      redirectLink: "https://jsonplaceholder.typicode.com",
-      category: "Design",
-    },
-    {
-      id: 5,
-      title: "natus nisi omnis corporis facere molestiae rerum in",
-      url: "https://via.placeholder.com/600/f66b97",
-      userImage: "https://via.placeholder.com/150/f66b97",
-      redirectLink: "https://jsonplaceholder.typicode.com",
-      category: "Design",
-    },
   ];
   humburger();
   headerAppearsWithScroll();
-  paginator(data);
+  paginator(dataForPaginator);
+  $(".course__slider-wrap").slick({
+    slidesToShow: 3,
+    speed: 500,
+    prevArrow: document.querySelector(".course__slider-btn_left"),
+    nextArrow: document.querySelector(".course__slider-btn_right"),
+
+    responsive: [
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 3,
+          variableWidth: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          variableWidth: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          variableWidth: true,
+        },
+      },
+      {
+        breakpoint: 375,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+          variableWidth: true,
+        },
+      },
+    ],
+  });
 });
