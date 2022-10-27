cea5784cb448edb4c6f1f07408d8597610e8c67f
Lutsenko Bohdan <lutsenko2004@gmail.com>
2022-10-27 16:41:34 +0300

    ls_8 add fetch and class select and corrected bugs prod version

--- html_css/src/js/select.js
+++ html_css/src/js/select.js
@@ -9,6 +9,8 @@ export class Select {
     this.list;
     this.label;
     this.span;
+    this.arrow;
+    this.quantitiOfAlbumId = 0;
   }
   onChange(value) {
     this.changeCallback(value, this.data);
@@ -30,6 +32,7 @@ export class Select {
     for (let i = 0; i < result.length; i++) {
       if (id != result[i].albumId) {
         count = 0;
+        this.quantitiOfAlbumId += 1;
       }
       if (count == 7) {
         continue;
@@ -49,7 +52,7 @@ export class Select {
     return `
     <button class="prefer__button">
         <span class="prefer__button_text">Label</span>
-        <i class="prefer__arrow-down"></i>
+        <i class="prefer__arrow"></i>
     </button>
     <ul class="prefer__list"></ul>
     `;
@@ -58,12 +61,17 @@ export class Select {
     this.list = document.querySelector(".prefer__list");
     this.label = document.querySelector(".prefer__button");
     this.span = document.querySelector(".prefer__button_text");
+    this.arrow = document.querySelector(".prefer__arrow");
   }
   initList() {
     let item = document.querySelector(this.select);
     item.innerHTML = this.templateButtonAndList();
     this.initVariables();
-    this.labelClickHandler();
+    if (this.quantitiOfAlbumId != 2) {
+      this.labelClickHandler();
+    } else {
+      this.label.classList.add("prefer__button_disabled");
+    }
     this.addListItem();
   }
   addListItem() {
@@ -83,7 +91,9 @@ export class Select {
   }
   labelClickHandler() {
     this.label.addEventListener("click", () => {
+      this.label.classList.toggle("prefer__button_active");
       this.list.classList.toggle("prefer__list_show");
+      this.arrow.classList.toggle("prefer__arrow_active");
     });
     this.list.addEventListener("click", (e) => {
       let target = e.target;
