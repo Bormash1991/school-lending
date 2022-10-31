24fbe384af375a85ed07a5f29ed117a3f0d10080
Bormash1991 <53615723+Bormash1991@users.noreply.github.com>
2022-10-31 14:08:27 +0200

    Ls 8 (#18)
    * ls_8 add fetch and class select
    
    * ls_8 add fetch and class select prod version
    
    * ls_8 add fetch and class select and corrected bugs prod version
    
    * ls_8 finally

--- html_css/src/js/app.js
+++ html_css/src/js/app.js
@@ -8,17 +8,36 @@ import {
   dataForCoursesSlider,
   dataForSlider,
 } from "./data-for-sliders.js";
-import { Storage } from "./storage.js";
-
 import { initForm } from "./form";
+import { smoothScroll } from "./scroll.js";
+import { Select } from "./select";
 export class App {
-  constructor() {}
+  constructor() {
+    this.slider;
+    this.bool = true;
+  }
+  updateSlider(id, data) {
+    if (this.bool) {
+      this.slider = new PreferSlider(data);
+      this.slider.initSlider();
+      this.slider.setData(id);
+      this.bool = false;
+    } else {
+      this.slider.setData(id);
+    }
+  }
   init() {
     hamburger();
     headerAppearsWithScroll();
     paginator(dataForPaginator);
     coursesSlider(dataForCoursesSlider);
-    new PreferSlider(new Storage(dataForSlider).getSliderData()).initSlider();
     initForm();
+    smoothScroll();
+    let sel = new Select(
+      "https://jsonplaceholder.typicode.com/photos/",
+      this.updateSlider.bind(this)
+    );
+    sel.makeRequest();
+    
   }
 }
---
+++ html_css/src/js/select.js
@@ -0,0 +1,108 @@
+export class Select {
+  #select;
+  constructor(url, changeCallback) {
+    this.numberOflabel;
+    this.BASE_URL = url;
+    this.#select = ".prefer__label";
+    this.data;
+    this.changeCallback = changeCallback;
+    this.list;
+    this.label;
+    this.span;
+    this.arrow;
+    this.quantitiOfAlbumId = 0;
+  }
+  onChange(value) {
+    this.changeCallback(value, this.data);
+  }
+  makeRequest() {
+    fetch(this.BASE_URL)
+      .then((response) => response.json())
+      .then((result) => {
+        this.calculatingData(result);
+      });
+  }
+  get select() {
+    return this.#select;
+  }
+  calculatingData(result) {
+    let arr = [];
+    let id = 0;
+    let count = 0;
+    for (let i = 0; i < result.length; i++) {
+      if (id != result[i].albumId) {
+        count = 0;
+        this.quantitiOfAlbumId += 1;
+      }
+      if (count == 7) {
+        continue;
+      }
+      if (result[i].albumId <= 3) {
+        arr.push(result[i]);
+        count += 1;
+        id = result[i].albumId;
+      } else {
+        break;
+      }
+    }
+    this.data = arr;
+    this.initList();
+  }
+  templateButtonAndList() {
+    return `
+    <button class="prefer__button">
+        <span class="prefer__button_text">Label</span>
+        <i class="prefer__arrow"></i>
+    </button>
+    <ul class="prefer__list"></ul>
+    `;
+  }
+  initVariables() {
+    this.list = document.querySelector(".prefer__list");
+    this.label = document.querySelector(".prefer__button");
+    this.span = document.querySelector(".prefer__button_text");
+    this.arrow = document.querySelector(".prefer__arrow");
+  }
+  initList() {
+    let item = document.querySelector(this.select);
+    item.innerHTML = this.templateButtonAndList();
+    this.initVariables();
+    if (this.quantitiOfAlbumId != 2) {
+      this.labelClickHandler();
+    } else {
+      this.label.classList.add("prefer__button_disabled");
+    }
+    this.addListItem();
+  }
+  addListItem() {
+    let number = 0;
+    this.data.forEach((elem, i) => {
+      if (number == 0) {
+        this.list.innerHTML += this.labelTemplate(elem.albumId);
+      } else if (number != elem.albumId) {
+        this.list.innerHTML += this.labelTemplate(elem.albumId);
+      }
+      number = elem.albumId;
+    });
+    this.onChange(1);
+  }
+  labelTemplate(number) {
+    return `<li id="${number}" class="prefer__list-item">Label ${number}</li>`;
+  }
+  labelClickHandler() {
+    this.label.addEventListener("click", () => {
+      this.label.classList.toggle("prefer__button_active");
+      this.list.classList.toggle("prefer__list_show");
+      this.arrow.classList.toggle("prefer__arrow_active");
+    });
+    this.list.addEventListener("click", (e) => {
+      let target = e.target;
+      if (target.classList.contains("prefer__list-item")) {
+        this.list.classList.toggle("prefer__list_show");
+        this.numberOflabel = target.getAttribute("id");
+        this.span.textContent = target.textContent;
+        this.onChange(this.numberOflabel);
+      }
+    });
+  }
+}
